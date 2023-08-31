68c4d6bcea08df87e3a5fa1aa82ea6e7730795f9
Bappy <bappymodok0011@gmail.com>
2023-08-31 06:22:25 -0700

    unicke service id pass to the client side

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-console.log(process.env.DB_PASS)
 
 
 // Mongodb Conuntin 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jqnstr5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,12 +28,48 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const serviceCollation = client.db('serviceCar').collection('service');
+    const productCollaction = client.db('products').collection('product');
+
+    app.get('/services',async(req,res)=>{
+        const  cursor = serviceCollation.find();
+        const result = await cursor.toArray()
+        res.send(result); 
+
+    })
+
+    app.get('/products',async(req,res)=>{
+        const corsor = productCollaction.find();
+        const result = await corsor.toArray();
+        res.send(result)
+    })
+
+    app.get('/services/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+
+
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1,service_id:1 },
+      };
+
+
+
+      const result = await serviceCollation.findOne(query,options)
+      res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
